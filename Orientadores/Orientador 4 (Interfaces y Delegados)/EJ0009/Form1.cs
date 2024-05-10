6132408a714ccd3d3e66af2f9cdd383eeb26fa86
Gerardo Tordoya <zherar7ordoya@outlook.com>
2024-05-10 19:56:36 -0300

    2024-05-10

--- Orientadores/Orientador 4 (Interfaces y Delegados)/EJ0009/Form1.cs
+++ Orientadores/Orientador 4 (Interfaces y Delegados)/EJ0009/Form1.cs
@@ -11,26 +11,35 @@ using Microsoft.VisualBasic;
 
 namespace EJ0009
 {
+    // Declaración del Delegado
+    public delegate string Delegado(string texto);
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-        // Declaración del Delegado
-        public delegate string Delegado(String pTexto);
+
         // Declaración de la variable D del tipo Delegado (Delegado es un delegate)
-        Delegado D;
+        Delegado delegado;
+
         private void Form1_Load(object sender, EventArgs e)
         {
             // Intanciación. Se denomina así al hecho de asignarle 
-            D = UsoDelDelegado;
+            delegado = MetodoQueUsaraElDelegado;
         }
-        public string UsoDelDelegado(string pTexto) { MessageBox.Show("1" + pTexto); return pTexto; }
-        private void button1_Click(object sender, EventArgs e)
+
+        public string MetodoQueUsaraElDelegado(string texto)
         {
-            // llamada al Delegado.
-            MessageBox.Show(D(Interaction.InputBox("Ingrese un Texto: ")));
+            MessageBox.Show("1) " + texto);
+            return texto; 
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            // Llamada al delegado.
+            //MessageBox.Show(delegado(Interaction.InputBox("Ingrese un Texto: ")));
+            delegado(Interaction.InputBox("Ingrese un Texto: "));
         }
     }
 }
